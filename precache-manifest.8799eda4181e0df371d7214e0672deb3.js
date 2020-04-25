6fa95918c6f1d250779accb772cd58fc54ae0760
Teresita Madriaza <teresita@MacBook-Pro-de-Teresita.local>
2020-04-24 23:17:33 -0400

    Updates

---
+++ precache-manifest.8799eda4181e0df371d7214e0672deb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959c7521af485a30cf0a4a2135933c85",
+    "url": "/gasto-semanal/index.html"
+  },
+  {
+    "revision": "9557053c163734741e7b",
+    "url": "/gasto-semanal/static/css/main.a27ff935.chunk.css"
+  },
+  {
+    "revision": "4d4c3b070c906df81f87",
+    "url": "/gasto-semanal/static/js/2.4c9db2ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gasto-semanal/static/js/2.4c9db2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9557053c163734741e7b",
+    "url": "/gasto-semanal/static/js/main.e0b107d7.chunk.js"
+  },
+  {
+    "revision": "59c58eb936d4e676ead6",
+    "url": "/gasto-semanal/static/js/runtime-main.b8b17fa2.js"
+  }
+]);
